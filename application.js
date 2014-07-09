fe04aa43c4ca4549f12a6aa473d614a33ed4a85f
Tim <timcannady@gmail.com>
2014-07-08 21:22:09 -0700

    made some updates

--- application.js
+++ application.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){ 
 
-	function capitalizeFirstLetter(str) {
-	    return str.charAt(0).toUpperCase() + str.substring(1, str.length);
-	}
+	
+	//global funciton
+	//function capitalizeFirstLetter(str) {
+	//	return str.charAt(0).toUpperCase() + str.substring(1, str.length);
+	//	}
+	
+	//global function
+	//function capitalizeName(name){
+	   // var lowerName = name.toLowerCase();
+	  //  var spaceIndex = lowerName.indexOf(' ');
+	  //  var first = lowerName.substring(0, spaceIndex);
+	  //  var last = lowerName.substring(spaceIndex+1, name.length);
+	  //  return capitalizeFirstLetter(first) + " " + capitalizeFirstLetter(last);   
+	//	}
 
-	function capitalizeName(name){
-	    //The string below contains a lower case version of name. Let's use this from now on.
-	    var lowerName = name.toLowerCase();
+	//when the <submit> button is clicked
+	$('button').click(function(){
+	
+	//store the user's entry in a variable
+		var enteredName = document.getElementById('enteredFormName').value;
+		
+		//update the html in the second form to show the user's entered name
+		$('#fixedFormName').value(enteredName);
+	});
+}
 
-	    var spaceIndex = lowerName.indexOf(' ');
-	    var first = lowerName.substring(0, spaceIndex);
-	    var last = lowerName.substring(spaceIndex+1, name.length);
-	    
-	    return capitalizeFirstLetter(first) + " " + capitalizeFirstLetter(last);
-	    
-	}
-
-$('button').click(function(){
-	CapitalizeName("full name")
-
-});
+//When the user clicks SUBMIT, store their name (taken from form <enteredFormName>) in 
+//variable <enteredName>.
+//Use this variable in the function that capitalizes it. Store the result in variable <fixedName>.
+//Update the results form <fixedFormName> with the variable.
